6d2abd4a1ecd2af5184ee0cf2022f0458952e163
= <=>
2025-05-12 14:31:31 +0200

    despliegue

--- front/js/main.js
+++ front/js/main.js
@@ -12,7 +12,7 @@ let currentUser = null;
 let selectedPriority = 'all';
 
 // URL base para la API
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_URL = 'https://taskplannerpro-api.onrender.com/api';
 
 // Elementos DOM
 const taskColumns = {
@@ -86,7 +86,7 @@ const getCurrentUser = async () => {
   } catch (error) {
     localStorage.removeItem('token');
     window.location.href = 'html/login.html';
-
+    
     console.error('Error al obtener usuario:', error);
   }
 };
